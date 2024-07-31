72cf78ff71b40f163dba1a1480317d19a0ba765a
dylancsu <dylan.dean@csuglobal.edu>
2024-07-31 15:05:22 -0600

    Do part 2

--- part2.py
+++ part2.py
@@ -1,1 +1,10 @@
+currenttime = int(input("Enter the current time in hours since midnight: "))
 
+if(currenttime>24 | currenttime<0):
+    print("invalid starting time")
+else:
+    increase = int(input("Enter the number of hours to pass: "))
+    if(increase<0):
+        print("Invalid increase")
+    else:
+        print(f"The new time is {(currenttime+increase)%24}")
