0e349f6de705beda307b6a615973f1783d7b4f6f
dylancsu <dylan.dean@csuglobal.edu>
2024-07-31 14:58:30 -0600

    Don't be an idiot

--- part1.py
+++ part1.py
@@ -1,4 +1,4 @@
-pretax = float(input("Enter the restaurant cost"))
-tax = pretax * 1.07
-tip = pretax * 1.18
+pretax = float(input("Enter the restaurant cost: "))
+tax = pretax * 0.07
+tip = pretax * 0.18
 print(f"With a ${tax} tax and a ${tip} tip, the total comes to ${pretax+tax+tip}")
